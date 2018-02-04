af49a0928028111de65be416ba7c253b883e8e79
t-takasaka <jikottedappun@excite.co.jp>
2018-02-04 23:06:04 +0900

    Add models.

--- js/live2dcubismframework.js
+++ js/live2dcubismframework.js
@@ -1095,16 +1095,3 @@ var LIVE2DCUBISMFRAMEWORK;
     LIVE2DCUBISMFRAMEWORK.GroupBody = GroupBody;
 })(LIVE2DCUBISMFRAMEWORK || (LIVE2DCUBISMFRAMEWORK = {}));
 
-(function(){
-	var script=document.createElement('script');
-	script.onload=function(){
-		var stats=new Stats();
-		document.body.appendChild(stats.dom);
-		requestAnimationFrame(function loop(){
-			stats.update();
-			requestAnimationFrame(loop)
-		});
-	};
-	script.src='//rawgit.com/mrdoob/stats.js/master/build/stats.min.js';
-	document.head.appendChild(script);
-})();
